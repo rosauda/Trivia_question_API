1f41a4aac92c6c19a439b04750395cc0cc99f780
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-02-04 12:22:18 +0100

    Requesting data using Trivia API

---
+++ data.py
@@ -0,0 +1,21 @@
+import requests
+
+# ---------------------------- CONSTANTS ------------------------------- #
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+
+# ---------------------------- GETTING TRIVIA QUESTIONS USING API ------------------------------- #
+
+# Requesting data
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+
+# Raising errors and exceptions
+response.raise_for_status()
+
+# Data to json
+data = response.json()
+question_data = data["results"]
